2fbf20f23ebb8f42263fdc3dc979309d1687fe70
Nathaniel Slater <nslater@Nathaniels-MacBook-Pro.local>
2022-11-03 14:11:35 -0700

    init repo

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3
+
+ENV VIRTUAL_ENV=/opt/venv
+RUN python3 -m venv $VIRTUAL_ENV
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
+
+WORKDIR /app
+ADD requirements.txt ./
+ADD dummy.py ./
+
+RUN pip install --no-cache-dir --upgrade pip \
+  && pip install --no-cache-dir -r requirements.txt
+
+EXPOSE 5000
+CMD [ "python", "dummy.py"]
---
+++ dummy.py
@@ -0,0 +1,16 @@
+import json
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/dummy")
+def dummy():
+    payload = {
+        'message': 'test'
+    }
+    return json.dumps(payload)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
