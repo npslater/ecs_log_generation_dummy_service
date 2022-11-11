361882a36f165225899759ba11a3b30e1ab97fe1
Nathaniel Slater <nathanielslater@Nathaniels-MacBook-Pro-2.local>
2022-11-11 09:37:41 -0800

    added a bunch of new methods to test logging

--- dummy.py
+++ dummy.py
@@ -1,8 +1,27 @@
 import json
-from flask import Flask
+from flask import Flask, request
+import flask as fl
 import os
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
+app.logger.setLevel('DEBUG')
 
 @app.route("/")
 def default():
@@ -13,7 +32,39 @@ def dummy():
     payload = {
         'message': 'test'
     }
-    return json.dumps(payload), 200, {'ContentType': 'application/json'}
+    app.logger.debug(payload)
+    return payload, 200, {'ContentType': 'application/json'}
+
+@app.route("/badrequest", methods=['GET','POST'])
+def badrequest():
+    app.logger.error(json.loads(request.data))
+    fl.abort(400)
+
+@app.route("/nopermission", methods=['GET','POST'])
+def nopermission():
+    app.logger.error(json.loads(request.data))
+    fl.abort(403)
+
+@app.route("/internalerror", methods=['GET','POST'])
+def internalerror():
+    app.logger.error(json.loads(request.data))
+    fl.abort(500)
+
+@app.route("/copycat", methods=['POST'])
+def copycat():
+    if not request.data:
+        badrequest();
+    payload = json.loads(request.data)
+    payload['newKey'] = 'copycat added this key'
+    app.logger.info(payload)
+    return payload, 200, {'ContentType': 'application/json'}
+
+@app.route("/status/<code>")
+def status(code):
+    statusCode = int(code)
+    app.logger.info(statusCode)
+    if code != 200:
+        fl.abort(statusCode)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 80))
