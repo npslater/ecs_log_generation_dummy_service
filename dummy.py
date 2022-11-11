5204c2ab6d095cb923e1232cea87a771afcdcfa0
Nathaniel Slater <nathanielslater@Nathaniels-MacBook-Pro-2.local>
2022-11-11 11:14:47 -0800

    fixed log formatting

--- dummy.py
+++ dummy.py
@@ -2,23 +2,6 @@ import json
 from flask import Flask, request
 import flask as fl
 import os
-from logging.config import dictConfig
-
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
-    }
-})
 
 app = Flask(__name__)
 app.logger.setLevel('DEBUG')
